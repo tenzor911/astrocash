ba9a14a33d4b490b7720f5d555dc1ca614417f5e
Vladimir <data-box@mail.com>
2014-01-13 02:35:25 +0400

    html

--- index.php
+++ index.php
@@ -11,56 +11,61 @@
         echo "<form  action=handler.php method=post>";	
         echo "<table border=1 align=center>";
             echo "<tr>";
+                echo "<td>";    
+                    echo "<center><img src=img\plus.png onclick=increment_box1(document.getElementById('box1'));></center>";
+                echo "</td>";
+                echo "<td>";    
+                    echo "<center><img src=img\plus.png onclick=increment_box2(document.getElementById('box2'));></center>";
+                echo "</td>";
+                echo "<td>";    
+                    echo "<center><img src=img\plus.png onclick=increment_box3(document.getElementById('box3'));></center>";
+                echo "</td>";
+                echo "<td>";    
+                    echo "<center><img src=img\plus.png onclick=increment_box4(document.getElementById('box4'));></center>";
+                echo "</td>";
+                echo "<td>";    
+                    echo "<center><img src=img\plus.png onclick=increment_box5(document.getElementById('box5'));></center>";
+                echo "</td>";
+            echo "</tr>";     
+            echo "<tr>";
                 echo "<td>";
-                    echo "<input type=text size=2 maxlength=2 name=user1>";
+                    echo "<input type='text' value='1' id='box1'/>";
                 echo "</td>";
                 echo "<td>";
-                    echo "<input type=text size=2 maxlength=2 name=user2>";
+                    echo "<input type='text' value='1' id='box2'/>";
                 echo "</td>";
                 echo "<td>";
-                    echo "<input type=text size=2 maxlength=2 name=user3>";
+                    echo "<input type='text' value='1' id='box3'/>";
                 echo "</td>";
                 echo "<td>";
-                    echo "<input type=text size=2 maxlength=2 name=user4>";
+                    echo "<input type='text' value='1' id='box4'/>";
                 echo "</td>";
                 echo "<td>";
-                    echo "<input type=text size=2 maxlength=2 name=user5>";
+                    echo "<input type='text' value='1' id='box5'/>";
                 echo "</td>";
             echo "</tr>"; 
+            echo "<tr>";
+                echo "<td>";    
+                    echo "<center><img src=img\minus.png onclick=decrement_box1(document.getElementById('box1'));></center>";
+                echo "</td>";
+                echo "<td>";    
+                    echo "<center><img src=img\minus.png onclick=decrement_box2(document.getElementById('box2'));></center>";
+                echo "</td>";
+                echo "<td>";    
+                    echo "<center><img src=img\minus.png onclick=decrement_box3(document.getElementById('box3'));></center>";
+                echo "</td>";
+                echo "<td>";    
+                   echo "<center><img src=img\minus.png onclick=decrement_box4(document.getElementById('box4'));></center>";
+                echo "</td>";
+                echo "<td>";    
+                   echo "<center><img src=img\minus.png onclick=decrement_box5(document.getElementById('box5'));></center>";
+                echo "</td>";
+            echo "</tr>";  
         echo "</table>";  
         echo "<center><input type=submit value='make bet!'></center>";
-        echo "</form>";
+        echo "<center><input id=b3 type=button value='try your luck!'/></center>";
+        echo "</form>"; 
          ?>
-        <div>
-            <img src='img\plus.png' onclick="increment_box1(document.getElementById('box1'));"/>
-            <input type='text' value='1' id='box1'/>
-            <img src='img\minus.png' onclick="decrement_box1(document.getElementById('box1'));"/>
-        </div>
-        <div>
-            <img src='img\plus.png' onclick="increment_box2(document.getElementById('box2'));"/>
-            <input type='text' value='1' id='box2'/>
-            <img src='img\minus.png' onclick="decrement_box2(document.getElementById('box2'));"/>
-        </div>
-        <div>
-            <img src='img\plus.png' onclick="increment_box3(document.getElementById('box3'));"/>
-            <input type='text' value='1' id='box3'/>
-            <img src='img\minus.png' onclick="decrement_box3(document.getElementById('box3'));"/>
-        </div>
-        <div>
-            <img src='img\plus.png' onclick="increment_box4(document.getElementById('box4'));"/>
-            <input type='text' value='1' id='box4'/>
-            <img src='img\minus.png' onclick="decrement_box4(document.getElementById('box4'));"/>
-        </div>
-        <div>
-            <img src='img\plus.png' onclick="increment_box5(document.getElementById('box5'));"/>
-            <input type='text' value='1' id='box5'/>
-            <img src='img\minus.png' onclick="decrement_box5(document.getElementById('box5'));"/>
-        </div>
-        <div>
-            <img src='img\plus.png' onclick="increment_box6(document.getElementById('box6'));"/>
-            <input type='text' value='1' id='box6'/>
-            <img src='img\minus.png' onclick="decrement_box6(document.getElementById('box6'));"/>
-        </div>
         <script>
             function increment_box1(myInput) {
                 if (myInput.value <= 19) {
@@ -101,7 +106,7 @@
                 myInput.value = Math.max(1, (myInput.value - 1) || 0);
             }
         </script>
-        <script>
+       <script>
             function increment_box5(myInput) {
                 if (myInput.value <= 19) {
                     myInput.value = (+myInput.value + 1) || 0;
@@ -111,15 +116,22 @@
                 myInput.value = Math.max(1, (myInput.value - 1) || 0);
             }
         </script>
-        <script>
-            function increment_box6(myInput) {
-                if (myInput.value <= 19) {
-                    myInput.value = (+myInput.value + 1) || 0;
-                }
+        <script type="text/javascript">
+            function rand(){
+                var mn = 1;
+                var mx = 20;
+                var ran = mn + Math.floor(Math.random()*(mx+1-mn));
+                return ran;
             }
-            function decrement_box6(myInput) {
-                myInput.value = Math.max(1, (myInput.value - 1) || 0);
-            }
+            $("#b3").click(function(){
+                $("#box1").val(rand());
+                $("#box2").val(rand());
+                $("#box3").val(rand());
+                $("#box4").val(rand());
+                $("#box5").val(rand());
+                $("#box5").val(rand());
+                return false;
+            });
         </script>
     </body>
 </html>
